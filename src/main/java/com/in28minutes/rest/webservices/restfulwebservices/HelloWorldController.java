ea2402a914d30ba25f091173f35c416210f6b799
buentellopc <buentellopc@gmail.com>
2022-08-15 11:07:03 -0500

    Enhancing the Hello World Service to return a Bean

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,30 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Specify the annotation to register a rest controller (instead of controller annotation)
+@RestController
 public class HelloWorldController {
+
+//    GET
+//    URI - /hello-world
+//    method - "Hello World"
+//    The get request needs to be mapped to the URI, use the proper annotation to accomplish that
+//    RequestMapping annotation is a more general way of mapping HTTP verbs with URI's
+
+    @GetMapping(path = "/hello-world")
+    public String hello(){
+        return "Hello World";
+    }
+
+
+
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloBean(){
+        return new HelloWorldBean("hi!");
+    }
 
 }
