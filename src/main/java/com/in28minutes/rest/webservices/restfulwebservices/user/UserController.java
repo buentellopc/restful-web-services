20651a8b4b015563deeb2131a69f9bd8b67a6082
buentellopc <buentellopc@gmail.com>
2022-08-15 16:13:38 -0500

    Step 10 enhancing post method to return HTTP status code and location

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,9 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class UserController {
@@ -12,29 +16,36 @@ public class UserController {
     private UserDaoService usersDao;
 
 
-//    Retrieve all users
+    //    Retrieve all users
     @GetMapping(path = "/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return usersDao.findAll();
     }
 
-//    Retrieve a user
+    //    Retrieve a user
 //    Casting automatically from string to id?
 //    path variable is "1" or 1
 //    parameter to getUser accepts an int
     @GetMapping(path = "/users/{id}")
-    public User getUser(@PathVariable int id){
+    public User getUser(@PathVariable int id) {
         return usersDao.findOne(id);
     }
 
 
-//
+    //
 //    input - details of user
 //
-@PostMapping (path = "/users")
-public User createUser(@RequestBody User user){
+    @PostMapping(path = "/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         usersDao.save(user);
-    return user;
-}
+
+//        After CREATED, return URI based on current path
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 
 }
