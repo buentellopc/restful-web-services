7aef8764b3aed453fa278f9e7cdec7a6b042a727
buentellopc <buentellopc@gmail.com>
2022-08-15 15:37:20 -0500

    Step 09 implementing post method to create user resource

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,9 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +28,13 @@ public class UserController {
     }
 
 
-
+//
+//    input - details of user
+//
+@PostMapping (path = "/users")
+public User createUser(@RequestBody User user){
+        usersDao.save(user);
+    return user;
+}
 
 }
